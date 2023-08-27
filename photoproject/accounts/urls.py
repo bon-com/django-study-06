4af4c1df12dcfd34c2910330ebd94ed00c3df473
bon-com <iwatamasateru1111@gmail.com>
2023-08-27 17:23:34 +0900

    インポート文の修正

--- photoproject/accounts/urls.py
+++ photoproject/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
 
 
 # アプリ名の登録
@@ -8,6 +8,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
